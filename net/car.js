7504658f83247554ff1d897e579f0de95aea4587
ranjayzheng <ranjayzheng@tencent.com>
2018-10-03 21:37:09 +0800

    init project

---
+++ net/car.js
@@ -0,0 +1,62 @@
+var carList = [
+  {
+    "name": "奥迪",
+    "url": "http://p.pstatp.com/avatar/100x100/1dd5000048d6334c26b4.png",
+    "index": 0
+  },
+  {
+    "name": "奔驰",
+    "url": "http://p.pstatp.com/avatar/100x100/1dd50000190229abeec8.png",
+    "index": 1
+  },
+  {
+    "name": "宝马",
+    "url": "http://p.pstatp.com/avatar/100x100/1dd5000018fab5bd782b.png",
+    "index": 2
+  },
+  {
+    "name": "本田",
+    "url": "http://p.pstatp.com/avatar/100x100/1dd5000018fc7c108922.png",
+    "index": 3
+  },
+  {
+    "name": "别克",
+    "url": "http://p.pstatp.com/avatar/100x100/1dd5000018fefd5c26b7.png",
+    "index": 4
+  },
+  {
+    "name": "比亚迪",
+    "url": "http://p.pstatp.com/avatar/100x100/1dd5000018f0e7000aa7.png",
+    "index": 5
+  },
+  {
+    "name": "保时捷",
+    "url": "http://p.pstatp.com/avatar/100x100/1dd50000190122c26c84.png",
+    "index": 6
+  },
+  {
+    "name": "大众",
+    "url": "http://p.pstatp.com/avatar/100x100/1dd500001906761eb47e.png",
+    "index": 7
+  },
+  {
+    "name": "哈弗",
+    "url": "http://p.pstatp.com/avatar/100x100/1dd50000193811739d87.png",
+    "index": 8
+  },
+  {
+    "name": "兰博基尼",
+    "url": "http://p.pstatp.com/avatar/100x100/1dd50000191738275961.png",
+    "index": 9
+  },
+  {
+    "name": "雪佛兰",
+    "url": "http://p.pstatp.com/avatar/100x100/1dd50000196a38ee3f43.png",
+    "index": 10
+  },
+  {
+    "name": "现代",
+    "url": "http://p.pstatp.com/avatar/100x100/1dd50000196d2f0b9a05.png",
+    "index": 11
+  }
+]
